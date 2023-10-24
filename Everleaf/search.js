9dcc625e8b64a6daf6f049c9a7da6d67d4f18f38
Mohitkanyal <102403245+Mohitkanyal@users.noreply.github.com>
2023-10-24 12:31:33 +0530

    Update search.js

--- Everleaf/search.js
+++ Everleaf/search.js
@@ -1,21 +1,21 @@
 const search = () => {
 const searchbox= document.getElementById("search-item").value.toUpperCase();
-const storeitems= document.getElementById("product-list")
-  const product= document.querySelectorAll(".product")
+const storeitems= document.getElementById("searchproduct-list")
+  const searchproduct= document.querySelectorAll(".searchproduct")
   const pname= document.getElementById("h3")
 for (var i=0;i<pname.length;i++)
   {
-let match=product[i].getElementByTagName('h3')[0];
+let match=searchproduct[i].getElementByTagName('h3')[0];
     if(match)
     {
 let textvalue=match.textContent || match.innerHTML
       if(textvalue.toUpperCase().indexOf(searchbox)>-1)
       {
-product[i].style.display="";
+searchproduct[i].style.display="";
         
       }
       else{
-        product[i].style.display="none";
+        searchproduct[i].style.display="none";
       }
       
     }
