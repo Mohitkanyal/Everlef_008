5418a06355291a69cfd3e7076b943e4edffa63d6
Mohitkanyal <102403245+Mohitkanyal@users.noreply.github.com>
2023-10-25 20:20:53 +0530

    Update script.js

--- Everleaf/assets/js/script.js
+++ Everleaf/assets/js/script.js
@@ -30,3 +30,30 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 200 ? header.classList.add("active")
     : header.classList.remove("active");
 })
+const search = () => {
+const searchbox= document.getElementById("search-item").value.toUpperCase();
+const storeitems= document.getElementById("searchproduct-list")
+  const searchproduct= document.querySelectorAll(".searchproduct")
+  const pname= document.getElementById("h3")
+for (var i=0;i<pname.length;i++)
+  {
+let match=searchproduct[i].getElementByTagName('h3')[0];
+    if(match)
+    {
+let textvalue=match.textContent || match.innerHTML
+      if(textvalue.toUpperCase().indexOf(searchbox)>-1)
+      {
+searchproduct[i].style.display="";
+        
+      }
+      else{
+        searchproduct[i].style.display="none";
+      }
+      
+    }
+    
+  }
+
+
+
+}
